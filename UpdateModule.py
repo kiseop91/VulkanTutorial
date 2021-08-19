5b0830fb489604d705f027e8d79a25310a755d37
kiseop91 <kiseop91@naver.com>
2021-08-19 17:36:02 +0900

    M1 Mac (arm64) 빌드 구성 추가

--- UpdateModule.py
+++ UpdateModule.py
@@ -21,8 +21,29 @@ def WindowsPackageInstall():
     os.system(vcpkg +'integrate install')
 
 def MacPackageInstall():
-    os.system("brew install glfw")
-    os.system("brew install glm")
+    # print("Input Target Architecture : (ex : arm64-osx, x64-osx) : ")
+    # arch = input()
+    print("\nClone Vcpkg Package Manager...")
+    os.system('git clone https://github.com/microsoft/vcpkg.git ./vendor')
+
+    currentFolderPath = os.getcwd()
+    print("\nCurrent Directory Path : " + currentFolderPath)
+
+    print("\nGenerate Vcpkg execute file... ")
+    subprocess.call([currentFolderPath + '/vendor/bootstrap-vcpkg.sh'])
+
+    vcpkg = currentFolderPath + '/vendor/vcpkg '
+
+    print("\nInstall ThirdParty Libraries... ")
+    os.system(vcpkg + 'install vulkan:x64-osx')
+    os.system(vcpkg + 'install glm:x64-osx')
+    os.system(vcpkg + 'install glfw3:x64-osx')
+
+    os.system(vcpkg + 'install vulkan:arm64-osx')
+    os.system(vcpkg + 'install glm:arm64-osx')
+    os.system(vcpkg + 'install glfw3:arm64-osx')
+
+    os.system(vcpkg +'integrate install')
 
 operatingSystem = platform.system()
 print("OS : " + operatingSystem)
