efb08ade47bae3aecbbd62fba8c6e444603c0309
kiseop91 <kiseop91@naver.com>
2021-10-11 17:20:34 +0900

    [ImGui] 컬러스페이스 업데이트

--- src/main.cpp
+++ src/main.cpp
@@ -1645,7 +1645,13 @@ private:
     {
         for (const auto &availableFormat : availableFormats)
         {
-            if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
+            if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB &&
+             availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR &&
+              availableFormat.colorSpace == VK_FORMAT_B8G8R8A8_UNORM &&
+              availableFormat.colorSpace == VK_FORMAT_R8G8B8A8_UNORM &&
+              availableFormat.colorSpace == VK_FORMAT_B8G8R8_UNORM &&
+              availableFormat.colorSpace == VK_FORMAT_R8G8B8_UNORM  
+              )
             {
                 return availableFormat;
             }
